c45b08e347920587f30cd6d75c742f247581d70b
William Alievi <william@mesquita.com.br>
2025-09-09 21:54:39 -0300

    Agora tem o icone + e -

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ $(document).ready(function(){
         let elPergunta = $(this);
         let elResposta = elPergunta.next('.resposta');
 
-        $('.resposta').not(elResposta).slideUp(200);
+        $('.pergunta').not(elPergunta)
+            .removeClass('ativa')
+            .find('.icone')
+            .html('+');
 
+        elPergunta.toggleClass('ativa');
+
+        $('.resposta').not(elResposta).slideUp(200);
         elResposta.slideToggle();
+
+        let icone = elPergunta.find('.icone');
+        if(elPergunta.hasClass('ativa')){
+            icone.html("-");
+        }else{
+            icone.html("+");
+        }
     })
 
 });
